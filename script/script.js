6f02fd43eeb58ead48a568f94aad6bb44997f58f
Sri <name@example.com>
2025-02-24 17:58:20 +0100

    Commit changes

--- script/script.js
+++ script/script.js
@@ -65,28 +65,28 @@ function createPortfolioFromJSON() {
     let row = document.createElement("div");
     row.classList.add("row");
 
-    // Load the JSON file
-    fetch("data/portfolio.json")
-        .then((response) => response.json())
-        .then((data) => {
-            // Iterate through the JSON data and create HTML elements
-            data.forEach((item, index) => {
-                const card = document.createElement("div");
-                card.classList.add("col-lg-4", "mt-4");
-                card.innerHTML = `
-                    <div class="card portfolioContent">
-                    <img class="card-img-top" src="images/${item.image}"  alt="${item.alt}" style="width:100%">
-                    <div class="card-body">
-                        <h3 class="card-title">${item.title}</h3>
-                        <p class="card-text">${item.text}</p>
-                        <div class="text-center">
-                            <a href="${item.link}" class="btn btn-success">Lien</a>
-                        </div>
-                    </div>
-                </div>
-                `;
-
-                // Append the card to the current row
+     // Load the JSON file
+     fetch("data/portfolio.json")
+     .then((response) => response.json())
+     .then((data) => {
+         // Iterate through the JSON data and create HTML elements
+         data.forEach((item, index) => {
+             const card = document.createElement("div");
+             card.classList.add("col-lg-4", "mt-4");
+             card.innerHTML = `
+                  
+                 <div class="card portfolioContent">
+                 <img class="card-img-top" src="images/${item.image}"  alt="${item.alt}" style="width:100%">
+                 <div class="card-body">
+                     <h3 class="card-title">${item.title}</h3>
+                     <p class="card-text">${item.text}</p>
+                     <div class="text-center">
+                         <a href="${item.link}" class="btn btn-success">Lien</a>
+                     </div>
+                 </div>
+             </div>
+             `;
+           // Append the card to the current row
                 row.appendChild(card);
 
                 // If the index is a multiple of 3 or it's the last element, create a new row
